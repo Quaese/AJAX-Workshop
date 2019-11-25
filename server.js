bc826bc3bfbdabeb028ebc95a3b11aa2355204bd
Markus Höllein <mhoellein@epages.com>
2019-11-25 09:13:16 +0100

    Add example for POST request

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 
+var bodyParser = require("body-parser");
 // get response JSON
 const responseJSON = require("./assets/request.json");
+
+// body-parser configuration
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +16,17 @@ app.use(express.static(__dirname + "/public"));
 app.get("/json", (req, res) => {
   const id = req.query.id
     ? req.query.id
+    : responseJSON[Object.keys(responseJSON)[0]];
+  const response = responseJSON[id]
+    ? { ...responseJSON[id], error: false }
+    : { error: true, message: "Record not found!" };
+
+  res.json(response);
+});
+
+app.post("/json", (req, res) => {
+  const id = req.body.id
+    ? req.body.id
     : responseJSON[Object.keys(responseJSON)[0]];
   const response = responseJSON[id]
     ? { ...responseJSON[id], error: false }
